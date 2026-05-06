4c9e6347d5b08002fa5497bebf16721bbc898d43
sofdb <sofia.deblasi@studenti.unipd.it>
2026-05-06 17:46:52 +0200

    #5 Implementate funzioni e test 1-3. Test success.

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -2,3 +2,40 @@
 // SOFIA DE BLASI 2111014
 // GIOVANNI VISENTIN 2101064
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanPrinter {
+
+    private static final Map<Character, String> ASCII = new HashMap<>();
+
+    static {
+        ASCII.put('I',
+        " _____ \n" +
+        "|_   _|\n" +
+        "  | |  \n" +
+        "  | |  \n" +
+        " _| |_ \n" +
+        "|_____|\n"
+        );
+    }
+
+    public static String print(int number) {
+        String roman = IntegerToRoman.convert(number);
+        return printAsciiArt(roman);
+    }
+
+    private static String printAsciiArt(String roman) {
+        StringBuilder result = new StringBuilder();
+        for(char c : roman.toCharArray()) {
+            if(!ASCII.containsKey(c)) {
+                throw new IllegalArgumentException("ASCII art non definita");
+            }
+            result.append(ASCII.get(c));
+        }
+        return result.toString();
+    }
+}
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -2,3 +2,49 @@
 // SOFIA DE BLASI 2111014
 // GIOVANNI VISENTIN 2101064
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RomanPrinterTest {
+
+    private static final String I_ASCII = " _____ \n|_   _|\n  | |  \n  | |  \n _| |_ \n|_____|\n";
+
+    @Test
+    public void print1ShouldReturnAsciiI() {
+        // Arrange
+        int number = 1;
+
+        // Act
+        String result = RomanPrinter.print(number);
+
+        // Assert
+        assertEquals(I_ASCII, result);
+    }
+
+    @Test
+    public void print2ShouldReturnAsciiII() {
+        // Arrange
+        int number = 2;
+
+        // Act
+        String result = RomanPrinter.print(number);
+
+        // Assert
+        assertEquals(I_ASCII + I_ASCII, result);
+    }
+
+    @Test
+    public void print3ShouldReturnAsciiIII() {
+        // Arrange
+        int number = 3;
+
+        // Act
+        String result = RomanPrinter.print(number);
+
+        // Assert
+        assertEquals(I_ASCII + I_ASCII + I_ASCII, result);
+    }
+}
